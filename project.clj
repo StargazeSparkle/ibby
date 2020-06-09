de45dca8e48640a5f3fb8119c768bd3531feb638
StargazeSparkle <stargaze.sparkle@gmail.com>
2020-06-09 17:58:53 -0400

    Updated README as well as license changes.

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject ibby "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :description "ibby (lowercase) is a project aimed at creating a MediaWiki client in Clojure that targets the Fandom installations."
+  :url "https://github.com/StargazeSparkle/ibby"
+  :license {:name "GPL-3.0"
+            :url "https://www.gnu.org/licenses/gpl-3.0.en.html"}
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [clj-http "3.10.1"]
                  [org.clojure/data.json "1.0.0"]]
